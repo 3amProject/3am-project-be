c1ec0091911428c58187b89f14ae5e443fd83b25
hflug8201 <jygassa@gmail.com>
2022-01-04 10:53:10 +0900

    security 로그인 config 생성

---
+++ src/main/java/com/tam/threeam/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.tam.threeam.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.tam.threeam.config.auth.PrincipalDetailService;
+
+
+
+
+/**
+ * @author 전예지
+ * @version 1.0
+ * @Description
+ * @Modification Information
+ * Created 2021/12/30
+ * @
+ * @ 수정일       수정자                   수정내용
+ * @ ———    ————    —————————————
+ * @ 2021/12/30     전예지       최초 작성
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private PrincipalDetailService principalDetailService;
+	
+	@Bean
+	public BCryptPasswordEncoder encoderPwd() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailService).passwordEncoder(encoderPwd());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.authorizeRequests()
+				.antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**") // TODO permitAll 입력
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+			.and()
+				.formLogin()
+				.loginPage("/auth/loginForm")
+				.loginProcessingUrl("/auth/loginProc")
+				.defaultSuccessUrl("/");
+	}
+	
+}
