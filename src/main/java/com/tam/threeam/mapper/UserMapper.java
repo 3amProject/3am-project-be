f20e76c939d9ad4da8e9c4a74becca56ff991c46
hflug8201 <jygassa@gmail.com>
2022-01-04 10:54:49 +0900

    유저 정보 수정 mapper 생성

--- src/main/java/com/tam/threeam/mapper/UserMapper.java
+++ src/main/java/com/tam/threeam/mapper/UserMapper.java
@@ -1,6 +1,10 @@
 package com.tam.threeam.mapper;
 
+import java.util.Optional;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.tam.threeam.model.User;
 
 /**
  * @author 이동은
@@ -11,8 +15,19 @@ import org.apache.ibatis.annotations.Mapper;
  * @
  * @ 수정일         수정자                   수정내용
  * @ ———    ————    —————————————
- * @ 2021/12/30     최초 작성
+ * @ 2021/12/30		이동은     최초 작성
+ * @ 2022/1/3			전예지		유저 정보 수정
  */
 @Mapper
 public interface UserMapper {
+	
+	// 유저 찾기
+	Optional<User> findUser(String userId);
+	
+	// 유저 정보 조회
+	
+	
+	
+	// 유저 정보 수정
+	int updateUserInfo(User user);
 }
