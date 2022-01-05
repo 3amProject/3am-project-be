ab1ee846db7f8dc68076d9d7bbf5268808383299
dongcholes <dongeunleee@gmail.com>
2022-01-05 15:47:14 +0900

    webConfig 추가

---
+++ src/main/java/com/tam/threeam/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.tam.threeam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    //운영 환경에 배포할 경우에는 주석 처리
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowCredentials(true).allowedOrigins("http://localhost:3000");
+    }
+
+}
