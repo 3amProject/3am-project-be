c7796475a81da1b4d8abc9155820537099bb938d
hluf8201 <jygassa@gmail.com>
2022-01-20 16:07:06 +0900

    security 로그인 JWT 토큰 관련 클래스 생성

---
+++ src/main/java/com/tam/threeam/model/JwtTokenRequest.java
@@ -0,0 +1,16 @@
+package com.tam.threeam.model;
+
+/**
+ * @author 전예지
+ * @version 1.0
+ * @Description
+ * @Modification Information
+ * Created 2022/01/19
+ * @
+ * @ 수정일       수정자                   수정내용
+ * @ ———    ————    —————————————
+ * @ 2022/01/19     전예지       최초 작성
+ */
+public class JwtTokenRequest {
+
+}
