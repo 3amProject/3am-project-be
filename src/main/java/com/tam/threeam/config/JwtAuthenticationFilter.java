9469d33ac0fab50504866cdcb7a4c048bcbe023e
dongcholes <dongeunleee@gmail.com>
2022-01-27 14:05:02 +0900

    필터 추가

--- src/main/java/com/tam/threeam/config/JwtAuthenticationFilter.java
+++ src/main/java/com/tam/threeam/config/JwtAuthenticationFilter.java
@@ -7,6 +7,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.tam.threeam.config.auth.PrincipalDetailService;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
@@ -16,17 +24,62 @@ import org.springframework.web.filter.OncePerRequestFilter;
  * @Modification Information
  * Created 2022/01/19
  * @
- * @ 수정일       수정자                   수정내용
- * @ ———    ————    —————————————
- * @ 2022/01/19     전예지       최초 작성
+ * @ 수정일     	 	 수정자        수정내용
+ * @ ———   			 ————    —————————————
+ * @ 2022/01/19      전예지       최초 작성
+ * @ 2022/01/27		 이동은
  */
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
+	@Autowired
+	private PrincipalDetailService principalDetailService;
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+
+		final String requestTokenHeader = request.getHeader("Authorization");
+
+		String userId = null;
+		String jwtToken = null;
+
+		if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+			jwtToken = requestTokenHeader.substring(7);
+			try {
+				userId = jwtTokenUtil.getUserIdFromToken(jwtToken);
+			} catch (IllegalArgumentException e) {
+				System.out.println("Unable to get JWT Token");
+			} catch (ExpiredJwtException e) {
+				System.out.println("JWT Token has expired");
+			}
+		}
+		else {
+			System.out.println("JWT Token does not begin with Bearer String");
+		}
+
+		// Once we get the token validate it.
+		if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+			UserDetails userDetails = this.principalDetailService.loadUserByUsername(userId);
+
+			// if token is valid configure Spring Security to manually set authentication
+			if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+						userDetails, null, userDetails.getAuthorities());
+				usernamePasswordAuthenticationToken
+						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				// After setting the Authentication in the context, we specify
+				// that the current user is authenticated. So it passes the
+				// Spring Security Configurations successfully.
+				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+			}
+		}
+		chain.doFilter(request, response);
 	}
 
 }
